aaece6d41cbe920274dff559f6e92f85717c885b
Vuk <vuchina@icloud.com>
2019-09-10 05:57:02 -0700

    add

---
+++ localVar.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+<!--
+var myVar = "global"; // Declare a global var
+function checkscope() {
+    var myVar = "local"; // Declare a local var
+    document.write(myVar);
+}
+//-->
+</script>
